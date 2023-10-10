d2d6369e50c1166bf0d932ed753c549497c154fb
Kayla Harrison <kaylaharrison@Kaylas-MacBook-Air.local>
2023-10-10 16:16:41 -0400

    test render puppies in dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { puppyList } from './data.js'
+import { useState } from 'react'
 
 import './App.css'
 
+const [puppies, setPuppies] = useState(puppyList)
 
-export default function App(){console.log(puppyList) 
+export default function App(){
       return (
-            <div> </div>
+            <div className="App">
+            {
+              puppies.map((puppy) => {
+                   return <p key={puppy.id}>{puppy.name}</p>;
+                 })
+             }
+          </div>
       );
 
 }
